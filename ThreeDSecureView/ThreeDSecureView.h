a97b6d483a60c242ea33d74fe6a932d2d959e918
Chris Leversuch <chris@brightec.co.uk>
2019-03-25 15:12:27 +0000

    Add framework project

---
+++ ThreeDSecureView/ThreeDSecureView.h
@@ -0,0 +1,19 @@
+//
+//  ThreeDSecureView.h
+//  ThreeDSecureView
+//
+//  Created by Chris Leversuch on 25/03/2019.
+//  Copyright © 2019 Brightec. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ThreeDSecureView.
+FOUNDATION_EXPORT double ThreeDSecureViewVersionNumber;
+
+//! Project version string for ThreeDSecureView.
+FOUNDATION_EXPORT const unsigned char ThreeDSecureViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ThreeDSecureView/PublicHeader.h>
+
+
